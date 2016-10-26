6888212644be488cac877596a97d9f0d6b05ea62
gabdsg <gabdsgs@hotmail.com>
2016-10-26 11:09:56 -0300

    fix

--- package.js
+++ package.js
@@ -1,7 +1,9 @@
 Package.describe({
+  name: 'gabdsg:reactive-fullcalendar',
   summary: "Meteor Reactive - Fullcalendar.io v3.0.1",
   version: "1.0.0",
-  git: "https://github.com/gabdsg/reactive-fullcalendar.git"
+  git: "https://github.com/gabdsg/reactive-fullcalendar.git",
+  documentation: 'README.md'  
 });
 
 Package.onUse(function(api) {
