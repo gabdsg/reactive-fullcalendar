12888b0c75d37c64d462344b58610ba57e6b6fe0
gabdsg <gabdsgs@hotmail.com>
2016-10-26 11:58:43 -0300

    fix

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'gabdsg:reactive-fullcalendar',
   summary: "Meteor Reactive - Fullcalendar.io v3.0.1",
-  version: "1.0.34",
+  version: "1.0.4",
   git: "https://github.com/gabdsg/reactive-fullcalendar.git",
   documentation: 'README.md'  
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
-  api.use('mrt:jquery-ui@1.9.2', 'client');
-  api.use('mrt:moment@2.8.1', 'client');
   api.use('reactive-var', 'client');
   api.use('templating', 'client');
   api.addFiles([
+      'lib/jquery.min.js',
+      'lib/moment.min.js',
       'fullcalendar.min.js',
       'fullcalendar.min.css',
-      'lang-all.js',
+      'locale-all.js',
       'gcal.js',
       'reactive-fullcalendar.js',
       'reactive-fullcalendar-template.html',
