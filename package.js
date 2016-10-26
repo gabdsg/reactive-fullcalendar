a34345ac8e03b3dc1f281ec5b9e5b39e727ddf97
gabdsg <gabdsgs@hotmail.com>
2016-10-26 11:46:34 -0300

    fix

--- package.js
+++ package.js
@@ -8,14 +8,14 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@1.0');
-  api.use('mrt:jquery-ui', 'client');
-  api.use('mrt:moment', 'client');
+  api.use('mrt:jquery-ui@1.9.2', 'client');
+  api.use('mrt:moment@2.8.1', 'client');
   api.use('reactive-var', 'client');
   api.use('templating', 'client');
   api.addFiles([
       'fullcalendar.min.js',
       'fullcalendar.min.css',
-      'locale-all.js',
+      'lang-all.js',
       'gcal.js',
       'reactive-fullcalendar.js',
       'reactive-fullcalendar-template.html',
